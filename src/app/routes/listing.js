a24ac80239ce5abfd4ae8e0958d5f2f2a85b6247
firllyw <fwahyudi17@gmail.com>
2021-10-01 17:35:01 +0700

    feat: publish route

--- src/app/routes/listing.js
+++ src/app/routes/listing.js
@@ -28,6 +28,10 @@ router.post('/listings',
     handlerException(tokenValidator),
     handlerException(listingController.insert));
 
+router.patch('/listings/:id/publish',
+    handlerException(tokenValidator),
+    handlerException(listingController.publish));
+
 router.patch('/listings/:id',
     multi,
     handlerException(tokenValidator),
