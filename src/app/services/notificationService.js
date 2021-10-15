9f1ad33ed21c7ea384647ba4d2fbd0b183558ff9
firlyw17 <firly@cloud-ace.com>
2021-10-15 23:17:23 +0700

    feat: aws ses setup

--- src/app/services/notificationService.js
+++ src/app/services/notificationService.js
@@ -1,5 +1,6 @@
 const notification = require('../models/notification');
 const user = require('../models/user');
+const mail = require('../config/mail');
 
 /**
  * @param {Object} self
@@ -46,22 +47,42 @@ async function itemSold(listing, socket) {
  * @param {Object} socket
  */
 async function priceChange(listing, socket) {
+  const addresses = [];
+  const notif = {
+    title: `${listing.name}: price has changed`,
+    event: 'Price Change',
+    listing: listing._id,
+    createdAt: Date.now(),
+  };
   for (const usrID of listing.subscribers) {
     const usr = await user.findById(usrID);
-    console.log(usr.notifications.priceChange);
     if (usr.notifications.priceChange) {
-      await notification.create({
-        title: `${listing.name}: price has changed`,
-        event: 'Price Change',
-        listing: listing._id,
-        userID: usr._id,
-        createdAt: Date.now(),
-      });
+      notif['userID'] = usr._id,
+      await notification.create(notif);
+      addresses.push(usr.email);
       await socket.to(usr._id.toString()).emit('priceChange', listing);
-
-      // Send Email Here
     }
   }
+  // Send Email Here
+  console.log('sending email');
+  mail.sendEmail({
+    Destination: {
+      ToAddresses: addresses,
+    },
+    Message: {
+      Subject: {
+        Data: notif.title,
+      },
+      Body: {
+        Text: {
+          Data: notif.title,
+        },
+      },
+    },
+    Source: 'fwahyudi17@gmail.com',
+  }).promise().then(function(data) {
+    console.log(data, 'emails sent');
+  });
 }
 
 module.exports = {
