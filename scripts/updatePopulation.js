e10e9f24c752773bba3e4ed6d4189a7d692e2270
firlyw17 <firly@cloud-ace.com>
2021-11-13 09:20:35 +0700

    feat: new script update city

--- scripts/updatePopulation.js
+++ scripts/updatePopulation.js
@@ -11,20 +11,20 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error: '));
 db.once('open', function() {
   console.log('Connected successfully');
-});
 
-fs.createReadStream(path.resolve(__dirname, 'update_population.csv'))
-    .pipe(csv.parse({headers: true}))
-    .on('data', (row) => {
-      db.collection('cities').findOneAndUpdate(
-          {'name': regexLike(row.name)},
-          {population: row.pop2021})
-          .then(function(data) {
-            console.log(data);
-          });
-    })
-    .on('end', (rowCount) => {
-      console.log('done', rowCount);
-    });
+  fs.createReadStream(path.resolve(__dirname, 'update_population.csv'))
+      .pipe(csv.parse({headers: true}))
+      .on('data', (row) => {
+        db.collection('cities').findOneAndUpdate(
+            {'name': regexLike(row.name)},
+            {$set: {population: row.pop2021}})
+            .then(function(data) {
+              console.log(row.name, row.pop2021, data.value.name);
+            });
+      })
+      .on('end', (rowCount) => {
+        console.log('done', rowCount);
+      });
+});
 
 
