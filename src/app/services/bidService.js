932db4c6a5172f99c7d79c19fa231e1f2895eb6b
firly <firly@cloud-ace.com>
2022-02-04 22:16:56 +0700

    feat: blockchain field on bids/me

--- src/app/services/bidService.js
+++ src/app/services/bidService.js
@@ -16,7 +16,7 @@ async function myBid(query, user) {
   const bids = await bidModel.paginate(filters,
       {populate: {
         path: 'listing',
-        select: 'name thumbnail tokenID',
+        select: 'name thumbnail tokenID blockchain',
       }});
   return bids;
 }
