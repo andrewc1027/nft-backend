af286d58c4d1644c652ec84af0903f64048382e6
firly <firly@cloud-ace.com>
2022-02-05 15:08:57 +0700

    wip: logger debug raw thumbnail

--- src/app/utils/s3.js
+++ src/app/utils/s3.js
@@ -35,6 +35,7 @@ async function upload(id, file, raw, socket, user) {
       ContentType: file.mimetype,
       ACL: 'public-read',
     };
+    console.log('Uplading: ', param);
     await s3.send(new PutObjectCommand(param));
   }
 
@@ -63,6 +64,7 @@ async function upload(id, file, raw, socket, user) {
       ContentType: 'image/jpeg',
       ACL: 'public-read',
     };
+    console.log('Uplading: ', rawParam);
     await s3.send(new PutObjectCommand(rawParam));
   }
 
@@ -132,6 +134,7 @@ async function processVideo(id, videoFile, newVidPath, type, options) {
           ContentType: 'image/gif',
           ACL: 'public-read',
         };
+        console.log('Uplading: ', param);
         await s3.send(new PutObjectCommand(param));
         updateListing(id, param.Key, '');
       } else if (type == 'mp4') {
@@ -143,6 +146,7 @@ async function processVideo(id, videoFile, newVidPath, type, options) {
           ContentType: 'video/mp4',
           ACL: 'public-read',
         };
+        console.log('Uplading: ', rawParam);
         await s3.send(new PutObjectCommand(rawParam));
         updateListing(id, '', rawParam.Key);
       }
@@ -158,10 +162,12 @@ async function processVideo(id, videoFile, newVidPath, type, options) {
 async function updateListing(id, key, rawKey) {
   const item = await listing.findById(id);
   if (key != '') {
+    console.log('Thumbnail ', key);
     item.thumbnail = `${process.env.AWS_BUCKET_URL}${key}` ?
       `${process.env.AWS_BUCKET_URL}${key}` : item.thumbnail;
   }
   if (rawKey != '') {
+    console.log('Raw Thumbnail ', rawKey);
     item.rawThumbnail = `${process.env.AWS_BUCKET_URL}${rawKey}` ?
       `${process.env.AWS_BUCKET_URL}${rawKey}` : item.rawThumbnail;
   }
@@ -197,8 +203,9 @@ async function upload360(id, thumbnail) {
     ContentType: thumbnail.mimetype,
     ACL: 'public-read',
   };
+  console.log('Uplading: ', param);
   const item = await s3.send(new PutObjectCommand(param));
-  updateListing(id, param.Key);
+  updateListing(id, param.Key, '');
   return item;
 }
 
