37d1f0f4257d01044bee31a19f045f8da5f15937
Vadim Prokoshin <v.a.prokoshin@gmail.com>
2022-02-27 00:07:51 +0200

    add script to append tokenId to tokenIds array for listings that have tokenID attr

--- scripts/attachTokenIds.js
+++ scripts/attachTokenIds.js
@@ -1,7 +1,4 @@
 const mongoose = require('mongoose');
-const csv = require('fast-csv');
-const fs = require('fs');
-const path = require('path');
 require('dotenv').config();
 
 console.log(process.env.MONGO_CONN);
@@ -11,21 +8,29 @@ db.on('error', console.error.bind(console, 'connection error: '));
 db.once('open', async () => {
     console.log('Connected successfully');
     let listingsToUpdate;
-    const cursor = db.collection('listings').find({"listingID": {$exists: true}});
+    let currentListing, listingsToUpdateNum;
+    let listingsUpdated = 0;
+    const cursor = db.collection('listings').find({"tokenID": {$exists: true}});
     if (await cursor.hasNext()) {
         listingsToUpdate = cursor.readBufferedDocuments();
-        let currentListing;
+        listingsToUpdateNum = listingsToUpdate.length
         for (currentListing of listingsToUpdate) {
-            if ('listingIds' in currentListing) {
-                if (currentListing.listingIds.indexOf(currentListing.listingID) === -1) {
-                    currentListing.listingIds.push(currentListing.listingID);
+            if ('tokenIds' in currentListing) {
+                if (currentListing.tokenIds.indexOf(currentListing.tokenID) === -1) {
+                    currentListing.tokenIds.push(currentListing.tokenID);
+                    await db.collection('listings').findOneAndUpdate({"_id": currentListing._id}, {$set: {"tokenIds": currentListing.tokenIds}})
+                    listingsUpdated++;
+                    console.log(`Listing ${currentListing._id} updated. tokenID ${currentListing.tokenID} added to tokenIds`)
+                } else {
+                    console.log(`tokenID ${currentListing.tokenID} for listing ${currentListing._id} already in tokenIds`)
                 }
+            } else {
+                currentListing.tokenIds = [currentListing.tokenID]
+                await db.collection('listings').findOneAndUpdate({"_id": currentListing._id}, {$set: {"tokenIds": currentListing.tokenIds}})
+                listingsUpdated++;
+                console.log(`Listing ${currentListing._id} updated`)
             }
-                console.log(currentListing)
         }
+        console.log(`Updated ${listingsUpdated} from ${listingsToUpdateNum} listings containing tokenId.`)
     }
-
-    console.log(listingsToUpdate)
 });
-
-console.log("exit")
