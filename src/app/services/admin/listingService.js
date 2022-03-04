3ade582f1491c8e5bf8af4382db25733b3a948cb
Vadim Prokoshin <v.a.prokoshin@gmail.com>
2022-03-04 17:48:51 +0200

    extend search

--- src/app/services/admin/listingService.js
+++ src/app/services/admin/listingService.js
@@ -1,11 +1,33 @@
 const listingModel = require("../../models/listing");
 const qTransform = require("../../utils/queryTransform");
+const {ObjectId} = require("bson");
+const user = require("../../models/user");
 
 async function getListings(userId, query, page, limit) {
     const queries = {};
 
-    if (query.search) {
-        queries['name'] = qTransform.regexLike(query.search);
+    if (query.deleted !== "true") {
+        queries['deleted'] = {$ne: true};
+    }
+
+    if (query.cityUrl) {
+        queries['city.url'] = query.cityUrl;
+    }
+
+    if (query.name) {
+        queries['name'] = qTransform.regexLike(query.name);
+    }
+
+    if (query.city) {
+        queries['city.ID'] = new ObjectId(query.city);
+    }
+
+    if (query.exclude) {
+        queries['_id'] = {$ne: new ObjectId(query.exclude)};
+    }
+
+    if (query.creator) {
+        queries['creator'] = new ObjectId(query.creator);
     }
 
     if (userId) {
