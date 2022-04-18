3b1cb542ea980a532dd83dc08c256e6e0924a902
firly <firly@cloud-ace.com>
2022-04-18 20:39:31 +0700

    feat: sortby admin trx, default date:desc

--- src/app/services/admin/trxService.js
+++ src/app/services/admin/trxService.js
@@ -5,11 +5,14 @@ const trxModel = require('../../models/transaction');
  * @param {Object} query 
  * @param {Number} page 
  * @param {Number} size 
+ * @param {String} sort
  */
-async function getTransactions(query, page, size) {
+async function getTransactions(query, page, size, sort) {
+    const sortBy = sort.split(':');
+    const orderBy = sortBy[1] === 'asc' ? '1' : '-1';
     const trxs = await trxModel.paginate({
         event: 'Purchasing', // Show Sell Transactions Only
-    }, {page: page, limit: size});
+    }, {page: page, limit: size, sort: {[sortBy[0]]: orderBy}});
     return trxs;
 }
 
